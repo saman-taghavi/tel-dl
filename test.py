087c79480ce6041629fbda4932e4b21ae5803778
saman <samantaghavisani@gmail.com>
2021-11-11 08:52:25 +0000

    Add a function to handle messaging

--- test.py
+++ test.py
@@ -11,7 +11,7 @@ import bot_replies
 
 # Import the client
 from telethon import TelegramClient, events, connection
-from telethon.tl import types
+from telethon import tl
 
 # Enable logging
 import logging
@@ -57,7 +57,7 @@ else:
 
 number_of_parallel_downloads = int(os.environ.get("TG_MAX_PARALLEL", 4))
 maximum_seconds_per_download = int(os.environ.get("TG_DL_TIMEOUT", 3600))
-proxy =  (server,port,secret)  # https://github.com/Anorov/PySocks
+proxy = (server, port, secret)  # https://github.com/Anorov/PySocks
 
 # date format
 fmt = "%Y-%m-%d %H:%M:%S"
@@ -74,9 +74,19 @@ download_detail = True
 tmp_path = os.path.join(download_path, "tmp")
 os.makedirs(tmp_path, exist_ok=True)
 
+# helper fuctions
+async def task_messenger(message: str, update: tl.custom.message.Message):
+    loop = asyncio.get_event_loop()
+    # and use the call back for progress of download
+    task = loop.create_task(update.reply(message))
+    # here we wait for the download to finish as function is async so no problem here
+    await asyncio.wait_for(task, timeout=30)
+
 
 async def worker(name):
     while True:
+        print(f"{name=}")
+        print(queue)
         # Get a "work item" out of the queue.
         downloaded_files = [
             f for f in listdir(download_path) if isfile(join(download_path, f))
@@ -84,8 +94,8 @@ async def worker(name):
         tmp_downloaded_files = [
             f for f in listdir(tmp_path) if isfile(join(tmp_path, f))
         ]
-        print(f"{downloaded_files=}")
-        print(f"{tmp_downloaded_files=}")
+        # print(f"{downloaded_files=}")
+        # print(f"{tmp_downloaded_files=}")
         queue_item = await queue.get()
         update = queue_item[0]
         reply = queue_item[1]
@@ -95,57 +105,64 @@ async def worker(name):
         # check disk space
         total, used, free = shutil.disk_usage("/")
         free = free // (1000 ** 3)
-        print(f"frees space = {free}")
-        print(f"{queue_files=}")
-        if free < 20:
-            await update.reply("less than 2 GB is left free some space")
-            return
-        # if file is downloaded or being downloaded tell the user
-        if file_name in downloaded_files:
-            await reply.edit("file is already downloaded")
-            return
-        if file_name in file_name in tmp_downloaded_files:
-            await reply.edit("file is downloading")
-            return
-        await reply.edit("Downloading...")
-        # convert time to ir local using pytz
-        print(
-            "[%s] Download started at %s" % (file_name, datetime.now(tz).strftime(fmt))
-        )
+        # print(f"frees space = {free}")
+        # print(f"{queue_files=}")
+
         try:
+            if free < 20:
+                await task_messenger("less than 2 GB is left free some space",update)
+                print('not free space')
 
-            async def progress_bar(current, total):
-                percentage = "{:.0f}%".format(current * 100 / total)
-                if (queue_files[file_name] != percentage) and download_detail:
-                    await reply.edit(f"{percentage}")
-                queue_files[file_name] = percentage
+            # if file is downloaded or being downloaded tell the user
+            elif file_name in downloaded_files:
+                await task_messenger("file is already downloaded",update)
+                print('in Downloaded')
 
-            loop = asyncio.get_event_loop()
-            # and use the call back for progress of download
-            task = loop.create_task(
-                client.download_media(
-                    update.message, file_path, progress_callback=progress_bar
+            elif file_name in file_name in tmp_downloaded_files:
+                await task_messenger("file is downloading",update)
+                print('in Temp')
+            else:
+                print('Downloading')
+                await reply.edit("Downloading...")
+                # convert time to ir local using pytz
+                print(
+                    "[%s] Download started at %s"
+                    % (file_name, datetime.now(tz).strftime(fmt))
                 )
-            )
-            # here we wait for the download to finish as function is async so no problem here
-            download_result = await asyncio.wait_for(
-                task, timeout=maximum_seconds_per_download
-            )
-            queue_files.pop(file_name)
-            end_time = datetime.now(tz).strftime(fmt)
-            _, filename = os.path.split(download_result)
-            final_path = os.path.join(download_path, filename)
-            # this moves the file
-            os.rename(download_result, final_path)
-            print(
-                "[%s] Successfully downloaded to %s at %s"
-                % (file_name, final_path, end_time)
-            )
-            await reply.edit("Finished at %s" % (end_time))
+
+                # i can move this out with a simpler function which pases more data to this
+                async def progress_bar(current, total):
+                    percentage = "{:.0f}%".format(current * 100 / total)
+                    if (queue_files[file_name] != percentage) and download_detail:
+                        await reply.edit(f"{percentage}")
+                    queue_files[file_name] = percentage
+
+                loop = asyncio.get_event_loop()
+                # and use the call back for progress of download
+                task = loop.create_task(
+                    client.download_media(
+                        update.message, file_path, progress_callback=progress_bar
+                    )
+                )
+                # here we wait for the download to finish as function is async so no problem here
+                download_result = await asyncio.wait_for(task, timeout=3)
+                queue_files.pop(file_name)
+                end_time = datetime.now(tz).strftime(fmt)
+                _, filename = os.path.split(download_result)
+                final_path = os.path.join(download_path, filename)
+                # this moves the file
+                os.rename(download_result, final_path)
+                print(
+                    "[%s] Successfully downloaded to %s at %s"
+                    % (file_name, final_path, end_time)
+                )
+                await reply.edit("Finished at %s" % (end_time))
+                queue.task_done()
+
         except asyncio.TimeoutError:
             print(
                 "[%s] Timeout reached at %s"
-                % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", datetime.now(tz).strftime(fmt)))
             )
             await reply.edit("Error!")
             message = await update.reply("ERROR: Timeout reached downloading this file")
@@ -154,7 +171,7 @@ async def worker(name):
             # print("[%s]: %s" % (e.__class__.__name__, str(e)))
             print(
                 "[%s] Exception at %s"
-                % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+                % (file_name, time.strftime("%Y-%m-%d %H:%M:%S", datetime.now(tz).strftime(fmt)))
             )
             await reply.edit("Error!")
             message = await update.reply(
@@ -162,7 +179,7 @@ async def worker(name):
                 % (e.__class__.__name__, str(e))
             )
 
-        # Notify the queue that the "work item" has been processed.
+            # Notify the queue that the "work item" has been processed.
         queue.task_done()
 
 
@@ -187,7 +204,7 @@ async def downloader(update):
     file_name = "unknown name"
     attributes = update.message.media.document.attributes
     for attr in attributes:
-        if isinstance(attr, types.DocumentAttributeFilename):
+        if isinstance(attr, tl.types.DocumentAttributeFilename):
             file_name = attr.file_name
             if file_name in list(queue_files):
                 # if file is in queue don't go further than this
@@ -201,6 +218,7 @@ async def downloader(update):
     )
     reply = await update.reply("In queue")
     await queue.put([update, reply, file_name])
+
 
 @events.register(events.NewMessage(pattern="/status"))
 async def get_status(update):
